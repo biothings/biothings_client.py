ad72d6de27bb9f147ef7099210875e2578dc24b4
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2017-02-10 08:06:19 -0800

    Adding query_by_uri and get_uri_list to jsonld_utils

--- biothings_client/utils/jsonld_utils.py
+++ biothings_client/utils/jsonld_utils.py
@@ -17,7 +17,7 @@ def find_top_level_uri(nquads_id, nquads, top_level_uris):
                 uri = item['predicate']['value']
             elif item['predicate']['value'] not in top_level_uris:
                 uri = find_top_level_uri(item['subject']['value'], nquads, top_level_uris)
-            else:
+            else: # this can never happen?
                 print("couldn't find top level uri")
     return uri
 
@@ -28,3 +28,26 @@ def fetch_value_source(client, _id, uri):
     source = [find_top_level_uri(item, nquads, client._top_level_jsonld_uris) for item in node]
     result = [i + ' ' + j for i,j in zip(value, source)]
     return result
+
+def get_uri_list(context):
+    uri_path_dict = {}
+    
+    for path, v in context.items():
+        for field_name, value in v["@context"].items():
+            new_path = path.replace("/", ".") + "." + field_name
+            if value not in uri_path_dict:
+                uri_path_dict[value] = [new_path]
+            else:
+                uri_path_dict[value].append(new_path)
+    return uri_path_dict
+
+def query_by_uri(client, uri, value, context):
+    context.pop('root')
+    query_string = ''
+    path_list = get_uri_list(context)[uri]
+    for _item in path_list:
+        query_string = query_string + ' ' + _item + ':' + value + ' OR'        
+    query_string = query_string.strip(' OR')
+    return client.query(query_string)
+
+
