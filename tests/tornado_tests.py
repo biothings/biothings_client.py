5985781f9e5b54dc8acb00b4b68125965c81f932
Chunlei Wu <anewgene@yahoo.com>
2017-04-26 09:39:45 -0700

    make tox tests work.

--- tests/tornado_tests.py
+++ tests/tornado_tests.py
@@ -1,12 +1,12 @@
-from tornado.testing import AsyncHTTPTestCase
+#from tornado.testing import AsyncHTTPTestCase
 from importlib import import_module
-from .gene import MyGeneTest
-from .variant import MyVariantTest
+from .gene import TestMyGenePy
+from .variant import TestMyVariantPy
 import argparse
 
 parser = argparse.ArgumentParser(description='Start test cases with HTTP server')
 parser.add_argument('--host', default=None, help='IP of gene ES index backend')
-parser.add_arument('--test_module', default=None, help='IP of variant ES index backend')
+parser.add_argument('--test_module', default=None, help='IP of variant ES index backend')
 
 #class ClientAsyncHTTPTest(TestMyGenePy, AsyncHTTPTestCase):
 
