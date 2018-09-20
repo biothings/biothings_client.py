695f9f2bdcf97380043e1e688161804968aaa826
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2018-09-20 16:37:54 -0400

    Making 'nose' a requirement for biothings_client

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="biothings_client",
-    version="0.1.1",
+    version="0.2.0",
     author="Cyrus Afrasiabi, Chunlei Wu",
     author_email="cwu@scripps.edu",
     description="Python Client for BioThings API services.",
@@ -39,6 +39,7 @@ setup(
     ],
     install_requires=[
         'requests>=2.3.0',
+        'nose',
     ],
     extras_require={
         'dataframe': ["pandas>=0.18.0"],
