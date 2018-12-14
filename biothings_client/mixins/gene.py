a94b964d39489ab1bf006b0f884eb71dcdd07de1
Chunlei Wu <anewgene@yahoo.com>
2018-12-13 21:21:32 -0800

    updated CHANGES.txt for v0.2.0 release

--- biothings_client/mixins/gene.py
+++ biothings_client/mixins/gene.py
@@ -2,7 +2,6 @@ class MyGeneClientMixin:
     def findgenes(self, id_li, **kwargs):
         '''.. deprecated:: 2.0.0
         Use :py:meth:`querymany` instead. It's kept here as an alias of :py:meth:`querymany` method.
-
         '''
         import warnings
         warnings.warn('Deprecated! Currently an alias of "querymany" method. Use "querymany" method directly.', DeprecationWarning)
