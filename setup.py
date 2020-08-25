3cd9b24b669c807c28699ef6ed6a324b09dc2a4d
Jerry <xzhou@scripps.edu>
2020-08-24 22:03:29 -0700

    Bump up version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="biothings_client",
-    version="0.2.1",
+    version="0.2.2",
     author="Cyrus Afrasiabi, Xinghua Zhou, Chunlei Wu",
     author_email="cwu@scripps.edu",
     description="Python Client for BioThings API services.",
