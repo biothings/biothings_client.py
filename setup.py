f62628a1639412b734017c4d2d8984005e42bcb9
Chunlei Wu <anewgene@yahoo.com>
2021-02-22 15:00:25 -0800

    included py3.8 in setup.py

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: BSD License",
         "Operating System :: POSIX",
