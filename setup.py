416315ffa78dc5b8afeba462be13096738222f7d
Jerry <xzhou@scripps.edu>
2021-02-22 14:35:06 -0800

    Add version update log

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="biothings_client",
-    version="0.2.3",
+    version="0.2.4",
     author="Cyrus Afrasiabi, Xinghua Zhou, Chunlei Wu",
     author_email="cwu@scripps.edu",
     description="Python Client for BioThings API services.",
