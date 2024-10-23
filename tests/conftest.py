d4e65c98c7a38db46b627b1991caf3041bc49680
jschaff <jschaff@scripps.edu>
2024-10-23 10:11:07 -0700

    Add async chem tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ Client fixtures for usage across the biothings_client testing
 import pytest
 
 from biothings_client import get_async_client
-from biothings_client.client.definitions import AsyncMyGeneInfo
+from biothings_client.client.definitions import AsyncMyGeneInfo, AsyncMyChemInfo
 
 
 @pytest.fixture(scope="session")
@@ -16,3 +16,13 @@ def async_gene_client() -> AsyncMyGeneInfo:
     client = "gene"
     gene_client = get_async_client(client)
     return gene_client
+
+
+@pytest.fixture(scope="session")
+def async_chem_client() -> AsyncMyChemInfo:
+    """
+    Fixture for generating an asynchronous mychem client
+    """
+    client = "chem"
+    chem_client = get_async_client(client)
+    return chem_client
