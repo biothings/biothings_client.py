691461acf76f20999d18c7fe66c3fd59ecf39c55
jschaff <jschaff@scripps.edu>
2024-10-28 13:45:51 -0700

    Update how we access importlib metadata

--- biothings_client/__version__.py
+++ biothings_client/__version__.py
@@ -1,5 +1,13 @@
-import importlib.metadata
+"""
+Package versioning using PEP-376
+https://peps.python.org/pep-0396/#pep-376-metadata
+"""
 
-# https://peps.python.org/pep-0396/#pep-376-metadata
-distribution_name = "biothings_client"
-__version__ = importlib.metadata.version(distribution_name)
+try:
+    from importlib import metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata  # type: ignore
+
+DISTRIBUTION_NAME = "biothings_client"
+__version__ = metadata.version(DISTRIBUTION_NAME)
