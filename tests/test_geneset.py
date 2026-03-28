ceb3c228486953cddbc8348fd34d3aa4960659e8
Chunlei Wu <anewgene@yahoo.com>
2026-03-27 17:02:53 -0700

    chore: prep for 0.5.0 version

--- tests/test_geneset.py
+++ tests/test_geneset.py
@@ -7,9 +7,8 @@ import types
 
 import pytest
 
-import biothings_client
-from biothings_client.utils.score import descore
 from biothings_client.client.definitions import MyGenesetInfo
+from biothings_client.utils.score import descore
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
